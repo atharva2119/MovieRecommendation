33f8b36f92fa57423929a11202e4e6df50cd7b77
atharva2119 <117750591+atharva2119@users.noreply.github.com>
2024-10-16 15:56:55 +0530

    Update movierecommendation.py

--- movierecommendation.py
+++ movierecommendation.py
@@ -19,119 +19,55 @@ Let's develop a basic recommendation system by suggesting items that are most si
 """
 
 import pandas as pd
-
 import numpy as np
-
-"""# **Import Dataset**"""
-
-df = pd.read_csv(r'https://raw.githubusercontent.com/YBIFoundation/Dataset/main/Movies%20Recommendation.csv')
-
-df.head()
-
-df.info()
-
-df.shape
-
-df.columns
-
-"""# **Get Feature Selection**"""
-
-df_features = df[['Movie_Genre','Movie_Keywords','Movie_Tagline','Movie_Cast','Movie_Director']].fillna('')
-
-df_features.shape
-
-df_features
-
-x = df_features['Movie_Genre']+' ' + df_features['Movie_Keywords'] + ' ' + df_features['Movie_Keywords'] + ' ' + df_features['Movie_Cast'] + ' ' + df_features['Movie_Director']
-
-x
-
-x.shape
-
-"""# **Get Feature text conversion to tokens**"""
-
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import difflib
+import streamlit as st
 
+# Load dataset
+df = pd.read_csv('https://raw.githubusercontent.com/YBIFoundation/Dataset/main/Movies%20Recommendation.csv')
+
+# Streamlit app title
+st.title("Movie Recommendation System")
+
+# Display dataset information
+if st.checkbox('Show Dataset Info'):
+    st.dataframe(df)
+
+# Feature selection
+df_features = df[['Movie_Genre', 'Movie_Keywords', 'Movie_Tagline', 'Movie_Cast', 'Movie_Director']].fillna('')
+x = df_features['Movie_Genre'] + ' ' + df_features['Movie_Keywords'] + ' ' + df_features['Movie_Tagline'] + ' ' + df_features['Movie_Cast'] + ' ' + df_features['Movie_Director']
+
+# Convert text to tokens
 tfidf = TfidfVectorizer()
-
 x = tfidf.fit_transform(x)
 
-x.shape
-
-print(x)
-
-from sklearn.metrics.pairwise import cosine_similarity
-
+# Calculate similarity score
 similarity_score = cosine_similarity(x)
 
-similarity_score
+# User input for favorite movie
+favourite_movie_name = st.text_input('Enter your favorite movie name:')
 
-similarity_score.shape
-
-"""# **Get Movie name from user and validate closest spelling**"""
-
-favourite_movie_name = input('Enter your favourite movie name:')
-
-All_movies_title_list = df['Movie_Title'].tolist()
-
-import difflib
-
-Movie_Recommendation = difflib.get_close_matches( favourite_movie_name, All_movies_title_list )
-print(Movie_Recommendation)
-
-Close_Match = Movie_Recommendation[0]
-print(Close_Match)
-
-Index_of_Close_Match_Movie =  df[df.Movie_Title == Close_Match]['Movie_ID'].values[0]
-print(Index_of_Close_Match_Movie)
-
-#getting list of similar movies
-Recommendation_Score = list(enumerate(similarity_score[Index_of_Close_Match_Movie]))
-print(Recommendation_Score)
-
-len(Recommendation_Score)
-
-"""# **Get All Movies Sorted Based on Recommendation Score wrt Favourite Movie**"""
-
-#sorting the movie based on their similarity score
-Sorted_Similar_Movies = sorted(Recommendation_Score, key = lambda x:x[1], reverse = True)
-print(Sorted_Similar_Movies)
-
-#print names of similar movies based on  index
-print("Top 30 movies suggested for you : ")
-
-i=1
-
-for movie in Sorted_Similar_Movies:
-  index = movie[0]
-  title_from_index = df[df.index==index]['Movie_Title'].values[0]
-  if (i<31):
-    print(i, '.',title_from_index)
-    i+=1
-
-"""# **Top 10 Movie Recommendation System**"""
-
-Movie_Name = input('Enter your favourite movie name: ')
-
-list_of_all_titles = df['Movie_Title'].tolist()
-
-Find_Close_Match = difflib.get_close_matches(Movie_Name, list_of_all_titles)
-
-Close_Match = Find_Close_Match[0]
-
-Index_of_Movie = df[df.Movie_Title == Close_Match]['Movie_ID'].values[0]
-
-Recommendation_Score = list(enumerate(similarity_score[Index_of_Movie]))
-
-sorted_similar_movies = sorted(Recommendation_Score, key = lambda x:x[1], reverse = True)
-
-print('Top 10 movies suggested for you :')
-
-i=1
-
-for movie in sorted_similar_movies:
-  index = movie[0]
-  title_from_index = df[df.Movie_ID==index]['Movie_Title'].values
-  if (i<11):
-    print(i, '.',title_from_index)
-    i+=1
+if favourite_movie_name:
+    All_movies_title_list = df['Movie_Title'].tolist()
+    Movie_Recommendation = difflib.get_close_matches(favourite_movie_name, All_movies_title_list)
+    
+    if Movie_Recommendation:
+        Close_Match = Movie_Recommendation[0]
+        Index_of_Close_Match_Movie = df[df.Movie_Title == Close_Match]['Movie_ID'].values[0]
+        
+        # Getting list of similar movies
+        Recommendation_Score = list(enumerate(similarity_score[Index_of_Close_Match_Movie]))
+        
+        # Sorting movies based on similarity score
+        Sorted_Similar_Movies = sorted(Recommendation_Score, key=lambda x: x[1], reverse=True)
+        
+        # Display recommendations
+        st.write("Top 10 movies suggested for you:")
+        for i, movie in enumerate(Sorted_Similar_Movies[1:11]):  # Exclude the first one as it's the input movie
+            index = movie[0]
+            title_from_index = df[df.index == index]['Movie_Title'].values[0]
+            st.write(f"{i+1}. {title_from_index}")
+    else:
+        st.write("No close match found for the given movie name.")
